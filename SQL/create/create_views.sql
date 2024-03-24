4bfd1322490002e7689cc8bc1cb303a43d9c86c8
James Xiao <jamesx.main@gmail.com>
2024-03-24 13:30:29 -0400

    Created, trained, and tested local lstm model for AMD

--- SQL/create/create_views.sql
+++ SQL/create/create_views.sql
@@ -1,14 +1,6 @@
 CREATE VIEW dm_tradinganalytics_sch.view_ml_training_data AS
 SELECT *
-FROM dw_tradinganalytics_sch.CalcFeatures('AMD', 'ml_data_1m_dw')
-WHERE EXTRACT(HOUR FROM datetime) >= 9
-  AND EXTRACT(HOUR FROM datetime) < 16
-  AND EXTRACT(MINUTE FROM datetime) >= 30
-ORDER BY datetime ASC;
-
-CREATE VIEW dm_tradinganalytics_sch.view_ml_training_data_5m AS
-SELECT *
-FROM dw_tradinganalytics_sch.CalcFeatures('AMD', 'ml_data_5m_dw')
+FROM dw_tradinganalytics_sch.CalcFeatures('AMD', 'ml_data_dw')
 WHERE EXTRACT(HOUR FROM datetime) >= 9
   AND EXTRACT(HOUR FROM datetime) < 16
   AND EXTRACT(MINUTE FROM datetime) >= 30
